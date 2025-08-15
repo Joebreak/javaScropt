1f3237d716edd87a7e8d3ca89ef68f6ac504ce2a
Joe <g132565@gmail.com>
2025-08-15 16:49:07 +0800

    update imdex page

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 function startGame() {
-  render();
+  const btn = document.getElementById('submitBtn');
+  if (btn) {
+    btn.onclick = () => {
+      const inputValue = document.getElementById('inputValue').value.trim();
+      if (inputValue) {
+        window.location.href = `first/${inputValue}.html`;
+      }
+    };
+  }
 }
 
 window.onload = () => {
