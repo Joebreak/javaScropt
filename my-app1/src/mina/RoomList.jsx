e3dc7ead0bf5f2222161f2786d47c7c51e08c5f6
Joe <g132565@gmail.com>
2025-09-05 10:33:59 +0800

    update list

--- my-app1/src/mina/RoomList.jsx
+++ my-app1/src/mina/RoomList.jsx
@@ -13,10 +13,11 @@ export default function RoomList({ data }) {
           overflow: "hidden",
         }}
       >
-       <div style={{ maxHeight: 200, overflowY: "auto" }}>
+        <div style={{ maxHeight: 200, overflowY: "auto" }}>
           <table style={{ borderCollapse: "collapse", width: "100%" }}>
             <thead>
               <tr>
+                <th style={{ position: "sticky", top: 0, background: "#4f8cff", color: "#fff", padding: "12px 8px" }}>#</th>
                 <th style={{ position: "sticky", top: 0, background: "#4f8cff", color: "#fff", padding: "12px 8px" }}>in</th>
                 <th style={{ position: "sticky", top: 0, background: "#4f8cff", color: "#fff", padding: "12px 8px" }}>out</th>
                 <th style={{ position: "sticky", top: 0, background: "#4f8cff", color: "#fff", padding: "12px 8px" }}>顏色</th>
@@ -25,6 +26,7 @@ export default function RoomList({ data }) {
             <tbody>
               {data.list.map((item, idx) => (
                 <tr key={idx}>
+                  <td style={{ padding: "12px 8px", borderTop: "1px solid #eaeaea", textAlign: "center", fontWeight: "bold", color: "#666" }}>{data.list.length - idx}</td>
                   <td style={{ padding: "12px 8px", borderTop: "1px solid #eaeaea", textAlign: "center" }}>{item.in}</td>
                   <td style={{ padding: "12px 8px", borderTop: "1px solid #eaeaea", textAlign: "center" }}>{item.out}</td>
                   <td style={{ padding: "12px 8px", borderTop: "1px solid #eaeaea", textAlign: "center" }}>{item.color}</td>
