1ee00c1e2a2f1983c64edfdcc36b92977a7fd3d0
Joe <g132565@gmail.com>
2025-09-25 12:07:36 +0800

    remove show size

--- my-app1/src/mine/MineRoom.jsx
+++ my-app1/src/mine/MineRoom.jsx
@@ -108,17 +108,6 @@ export default function MinaRoom() {
         list={data?.list || []}
       />
 
-      {/* 尺寸顯示 */}
-      <div style={{
-        textAlign: "center",
-        margin: "10px 0 5px 0",
-        fontSize: "12px",
-        color: "#666",
-        fontWeight: "bold"
-      }}>
-        目前尺寸: {window.innerWidth}px × {window.innerHeight}px 
-      </div>
-
       {/* 第一排按鈕：重新整理 + 顯示圖形 + 隱藏圖形 */}
       <div style={{
         display: "flex",
