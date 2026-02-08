5bb431b09bfcf6305eed6425ee33656106d8de95
Joehome <g132565@gmailcom>
2026-02-08 10:52:35 +0800

    update deploy

--- my-app1/src/index.js
+++ my-app1/src/index.js
@@ -5,13 +5,9 @@ import AppRoutes from "./routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// 從 PUBLIC_URL 獲取 basename（react-scripts 會自動從 homepage 設定）
-// 本地開發時 PUBLIC_URL 通常是空字串，GitHub Pages 時會是 /javaScropt
-const basename = process.env.PUBLIC_URL || '';
-
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <AppRoutes />
     </BrowserRouter>
   </React.StrictMode>
